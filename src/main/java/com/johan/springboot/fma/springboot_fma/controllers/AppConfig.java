b65f93d028a6b5a8934a289d1a2989e17f7006c8
johanfuertv <johanossa697@hotmail.com>
2024-08-06 01:14:19 -0500

    FMA Update

--- src/main/java/com/johan/springboot/fma/springboot_fma/controllers/AppConfig.java
+++ src/main/java/com/johan/springboot/fma/springboot_fma/controllers/AppConfig.java
@@ -11,16 +11,19 @@ import com.johan.springboot.fma.springboot_fma.models.User;
 @Configuration
 public class AppConfig {
 
-
-    //Primero creamos el metodo
-
     @Bean
     List<User> lista(){
         List<User> user = new ArrayList<>();
-        user.add(new User(null, null, null));
-        user.add(new User(null, null, null));
-        user.add(new User(null, null, null));
-        user.add(new User(null, null, null));
+        user.add(new User(1L, "Edward Elric", "Alchemist"));
+        user.add(new User(2L, "Alphonse Elric", "Armored Soul"));
+        user.add(new User(3L, "Roy Mustang", "Flame Alchemist"));
+        user.add(new User(4L, "Riza Hawkeye", "Lieutenant"));
+        user.add(new User(5L, "Winry Rockbell", "Mechanic"));
+        user.add(new User(6L, "Alex Louis Armstrong", "Strong Arm Alchemist"));
+        user.add(new User(7L, "Scar", "Avenger"));
+        user.add(new User(8L, "Maes Hughes", "Lieutenant Colonel"));
+        user.add(new User(9L, "Ling Yao", "Prince"));
+        user.add(new User(10L, "Greed", "Homunculus"));
         return user;
     }
 }
